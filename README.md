6d9cf2517fa4c41bcc1965fc4605982dedfce6ef
jeongnara <79076679+jeongnara@users.noreply.github.com>
2022-11-26 16:11:02 +0900

    models Notice

--- README.md
+++ README.md
@@ -15,4 +15,9 @@
     3. python manage.py runserver
 2. startapp notice
    1. python manage.py startapp notice
-   2. 'notice', in INSTALLED_APPS in settings.py 
+   2. 'notice', in INSTALLED_APPS in settings.py
+   3. models -> admin -> views -> templates -> urls
+   4. models
+      1. Notice
+      2. python manage.py makemigrations notice
+      3. python manage.py migrate notice  
--- notice/models.py
+++ notice/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+class Notice(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField(blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    modify_at = models.DateTimeField(auto_now=True)
 
-# Create your models here.
+    class Meta:
+        ordering = ('-modify_at', '-create_at', 'title')
+
+    def __str__(self):
+        return f'{self.title} | {self.content}'
