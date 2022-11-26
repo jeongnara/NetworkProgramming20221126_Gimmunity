a6ec339e249914736c7f7426f38decf062f80efd
jeongnara <79076679+jeongnara@users.noreply.github.com>
2022-11-26 17:05:43 +0900

    views, templates, urls notice:detail

--- notice/views.py
+++ notice/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from notice.models import Notice
 
 
 class NoticeListView(ListView):
     model = Notice
+
+class NoticeDetailView(DetailView):
+    model = Notice
+
 
